da3f6779395c1d48f70ba530702f77bbee70db45
hafify <hafify@airasia.com>
2021-11-29 20:19:10 +0800

    Add talents resources

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -6,4 +6,47 @@ class TalentsController < ApplicationController
   def show
     @talent = Talent.find(params[:id])
   end
+
+  def new
+    @talent = User.first.talents.new
+  end
+
+  def create
+    @talent = User.first.talents.new(talent_params)
+    if @talent.save
+      redirect_to @talent
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @talent = Talent.find(params[:id])
+  end
+
+  def update
+    @talent = Talent.find(params[:id])
+    if @talent.update(talent_params)
+      redirect_to @talent
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @talent = Talent.find(params[:id])
+    @talent.destroy
+    redirect_to talents_path
+  end
+
+  private
+
+  def talent_params
+    params.require(:talent).permit(
+      :name, :description,
+      :instagram_url, :instagram_handle,
+      :twitter_url, :twitter_handle,
+      images: []
+    )
+  end
 end
--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,12 +1,25 @@
 import { Controller } from '@hotwired/stimulus'
 
-import Swiper from "swiper"
+import Swiper from 'swiper/bundle'
 
 export default class extends Controller {
   connect() {
     console.log('swiper_controller.js')
     const swiper = new Swiper(".swiper-container", {
-      spaceBetween: 15
+      spaceBetween: 15,
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
     })
   }
 }
