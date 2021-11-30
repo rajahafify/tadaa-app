76bc4969c99a81211acac3c1f8fc95bdb1257e63
hafify <hafify@airasia.com>
2021-11-30 15:50:48 +0800

    Remove resizing

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -5,14 +5,4 @@ class Talent < ApplicationRecord
   has_many :talent_services
 
   has_many_attached :images
-
-  before_save :resize_images
-
-  private
-
-  def resize_images
-    images.each do |image|
-      image.variant(resize_and_pad: [400,500]).processed
-    end
-  end
 end
